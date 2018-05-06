8d61ef85cad6f2e379aad7ba60ed4a57308255be
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-05-06 17:39:03 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ def reverse_each_word(sentence)
   sentence_to_array = sentence.split(" ")
   reverse_sentence = ""
   sentence_to_array.each {|word| reverse_sentence.concat("#word.reverse}" )}
-  reverse_sentence.chop
+  reverse_sentence.chop 
 end
