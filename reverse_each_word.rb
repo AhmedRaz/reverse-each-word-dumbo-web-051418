b9c5d66b21a00c9e1637da046e4cdac028a8713b
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-05-06 17:43:39 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,6 @@
+def reverse_each_word(sentence)
+  sentence_to_array = sentence.split(" ")
+  
+  reverse_array = sentence_to_array.collect {|word| word.reverse}
+  reverse_sentenece = reverse_array.join(" ")
+end
